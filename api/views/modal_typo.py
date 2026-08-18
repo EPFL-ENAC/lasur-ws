4645a2c013d6181ac9d68bf71ff4053b59fb7c2a
ymarcon <yannick.marcon@epfl.ch>
2026-08-18 16:50:39 +0200

    fix: return bravos with pro recommendations

--- api/views/modal_typo.py
+++ api/views/modal_typo.py
@@ -214,7 +214,7 @@ async def compute_reco_pro(
     """Compute pro modal recommendation based on the provided data."""
     service = TypoModalService(od_mm, orig_dess, dest_dess, can_df)
     try:
-        reco_pros = service.compute_reco_pro_h3({
+        reco_pros, bravo_pros = service.compute_reco_pro_h3({
             'velo': data.score_velo,
             'tpu': data.score_tpu,
             'train': data.score_train,
@@ -224,7 +224,7 @@ async def compute_reco_pro(
             data.d_lat,
             data.d_lon
         )
-        return {'reco_pros': reco_pros}
+        return {'reco_pros': reco_pros, 'bravo_pros': bravo_pros}
     except Exception as e:
         logging.error(e, exc_info=True)
         return {'error': str(e)}
@@ -246,6 +246,7 @@ async def compute_empl_actions(
     except Exception as e:
         logging.error(e, exc_info=True)
         return {'error': str(e)}
+
 
 @router.post("/empl_v2", response_model=Dict)
 async def compute_empl_actions_v2(
